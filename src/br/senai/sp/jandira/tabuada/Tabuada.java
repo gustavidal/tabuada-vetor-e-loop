ea0df5d088300abe466ad78a221298750b4fd005
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2025-11-03 15:28:55 -0300

    Adição de função de obtenção de informações

--- src/br/senai/sp/jandira/tabuada/Tabuada.java
+++ src/br/senai/sp/jandira/tabuada/Tabuada.java
@@ -5,7 +5,7 @@ import br.senai.sp.jandira.tabuada.model.Usuario;
 public class Tabuada {
 
     public static void main(String[] args) {
-        System.out.println("Testando a calculadora...");
+        System.out.println("Iniciando a calculadora...");
 
         Usuario usuario = new Usuario();
 
--- src/br/senai/sp/jandira/tabuada/model/Usuario.java
+++ src/br/senai/sp/jandira/tabuada/model/Usuario.java
@@ -1,8 +1,28 @@
 package br.senai.sp.jandira.tabuada.model;
 
+import java.util.Scanner;
+
 public class Usuario {
 
-    public void obterInfomacoes(){
+    int multiplicando;
+    int multiplicadorInicial;
+    int multiplicadorFinal;
+    int resultado;
 
+    public void obterInfomacoes(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Informa de qual número será a tabuada: ");
+        multiplicando = leitor.nextInt();
+
+        System.out.print("Informe qual será o número a qual iniciará a multiplicação: ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.print("Informe qual será o número a qual terminará a multiplicação: ");
+        multiplicadorFinal = leitor.nextInt();
+
+        System.out.println(multiplicando);
+        System.out.println(multiplicadorInicial);
+        System.out.println(multiplicadorFinal);
     }
 }
